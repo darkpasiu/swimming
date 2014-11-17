95eb14f6d75392c8c365be51e8372ce7eb7bdfed
Blazej Mirowski <blazej.mirowski@tieto.com>
2014-11-17 10:11:30 +0100

    add TODOs
    Signed-off-by: Blazej Mirowski <blazej.mirowski@tieto.com>

--- src/CCompetition.h
+++ src/CCompetition.h
@@ -19,9 +19,9 @@ class CCompetition {
 	bool m_bGender;
 	vector<CRegistration *> m_RegisteredVector;
 
-	const unsigned int uitbl_scheme1[kiLineNumber6] = {1, 2, 3, 4, 5, 6};
-	const unsigned int uitbl_scheme2[kiLineNumber6] = {6, 5, 4, 3, 2, 1};
-	const unsigned int uitbl_scheme3[kiLineNumber6] = {3, 4, 2, 5, 1, 6};
+	const unsigned int kuitbl_scheme1[kiLineNumber6] = {1, 2, 3, 4, 5, 6};
+	const unsigned int kuitbl_scheme2[kiLineNumber6] = {6, 5, 4, 3, 2, 1};
+	const unsigned int kuitbl_scheme3[kiLineNumber6] = {3, 4, 2, 5, 1, 6};
 
 	public:
 	void printInfo();
